07fd06d0f6a09866debb87d8a594a32d6a8e127c
LAURANE SALIOU <laurane-saliou@orange.fr>
2015-12-07 12:13:39 +0000

    first attempt at node creation and sound generation

---
+++ js/BufferNode.js
@@ -0,0 +1,42 @@
+var BufferNode = fabric.util.createClass(fabric.Rect, {
+	type: 'buffer',
+
+	initialize: function(options){
+		options || (options = {});
+
+		this.callSuper('initialize', options);
+		this.set('url', options.url || '');
+		this.set('loop', options.loop || false);
+		this.set('label', options.label || '');
+	},
+
+	toObject: function(){
+		return fabric.util.object.extend(this.callSuper('toObject'), {
+			url: this.get('url'),
+			loop: this.get('loop'),
+			label: this.get('label')
+		});
+	},
+
+	_render: function(ctx) {
+    this.callSuper('_render', ctx);
+
+    ctx.font = '15px Helvetica';
+    ctx.fillStyle = '#333';
+    ctx.fillText(this.label, -this.width/2, -this.height/2 );
+  }
+});
+
+function BufferNod() {
+	var buff = new BufferNode({
+		left:200,
+		top:100,
+        width: 70,
+        height: 50,
+        fill: 'red',
+        opacity: 0.3,
+        label: "Buffer"
+	});
+
+	canvas.add(buff);
+}
---
+++ js/main.js
@@ -0,0 +1,86 @@
+var canvas = new fabric.Canvas('canvas');
+canvas.setHeight(window.innerHeight);
+canvas.setWidth(window.innerWidth);
+
+console.log("0");
+OscNode();
+console.log("1");
+BufferNod('hihat-plain.wav', true);
+console.log(JSON.stringify(canvas));
+console.log("2");
+
+
+canvas.on('object:selected', function(e){
+	var activeObject = e.target;
+	if (activeObject.get('type') === 'oscillator') {
+		activeObject.set('wave', window.prompt("Please enter a type of wave", "sine"));
+
+		activeObject.set('freq', window.prompt("Please enter a frequency", "200"));		
+		activeObject.set('duration', window.prompt("Please enter a duration", "2"));
+	} else if (activeObject.get('type') === 'buffer') {
+		activeObject.set('url', window.prompt("Please enter an url to a wav sound", "hihat-plain.wav"));
+		activeObject.set('loop', window.prompt("Please enter true if you want the sound to loop", "true"));		
+	}
+	
+});
+
+var ac = new AudioContext();
+var buf;
+
+function play(){
+	
+	var state = JSON.stringify(canvas);
+	var stateObj = JSON.parse(state);
+	
+	console.log(stateObj.objects[0]);
+		
+	var i, nbr; 
+	for (i = 0, nbr = stateObj.objects.length; i<nbr; i++){
+		console.log(stateObj.objects[i].type);
+		if (stateObj.objects[i].type === 'oscillator'){
+			var osc = ac.createOscillator();
+			osc.type = stateObj.objects[i].wave;
+			osc.frequency = stateObj.objects[i].freq;
+			osc.connect(ac.destination);
+			osc.start(ac.currentTime);
+			osc.stop(ac.currentTime + stateObj.objects[i].duration);
+		} else if (stateObj.objects[i].type === 'buffer'){
+			console.log(stateObj.objects[i].url);
+			startb(stateObj.objects[i].url, stateObj.objects[i].loop);
+		}
+	}
+}
+
+function getData(url, loop) {
+			  source = ac.createBufferSource();
+			  var request = new XMLHttpRequest();
+
+			  request.open('GET', url, true);
+
+			  request.responseType = 'arraybuffer';
+
+
+			  request.onload = function() {
+			    var audioData = request.response;
+
+			    ac.decodeAudioData(audioData, function(buffer) {
+			        source.buffer = buffer;
+
+			        source.connect(ac.destination);
+			        source.loop = loop;
+			      },
+
+			      function(e){"Error with decoding audio data" + e.err});
+
+			  }
+
+			  request.send();
+}
+var source;
+function startb(url, loop){
+	getData(url, loop);
+	source.start(0);
+}
+
+
+
